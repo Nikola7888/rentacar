872d6ca790cc84c8153b813edb60a128370327a8
Nikola <nikola.petrovic7888@gmail.com>
2026-08-15 09:37:49 +0200

    Dodati novi fajlovi u config folder

---
+++ app/config/config-example.php
@@ -0,0 +1,7 @@
+<?php
+const DB_HOST = 'localhost';
+const DB_USER = 'user';
+const DB_PASS = 'password';
+const DB_NAME = 'rentacar';
+const DB_PORT = '3308';
+const BASE_URL = 'http://localhost/rentacar/public/';
